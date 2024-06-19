9c72f6ce20ded7641b7fc368cbb249894d1cf197
Arthur Kunrath <57488934+arthurkth@users.noreply.github.com>
2024-06-19 15:04:42 +0000

    updated the `/status` endpoint to return the Postgres version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ mensagem: "OK" });
+  const updatedAt = new Date().toISOString();
+  const max_connections = await database.query("SHOW max_connections;");
+  const used_connections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
+  );
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVerionValue = databaseVersionResult.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVerionValue,
+      },
+    },
+  });
 }
 
 export default status;
